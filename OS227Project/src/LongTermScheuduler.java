c655f8642c9e184e5ecea983764b36ec3cb5cb47
Abdullah Alshammari <145904885+Abdu114hf16@users.noreply.github.com>
2024-11-09 19:43:19 +0300

    Update LongTermScheuduler.java

--- OS227Project/src/LongTermScheuduler.java
+++ OS227Project/src/LongTermScheuduler.java
@@ -1,4 +1,20 @@
 
 public class LongTermScheuduler {
 
-}
+    private Queue<PCB> readyQueue;
+
+    public Scheduler(Queue<PCB> readyQueue) {
+        this.readyQueue = readyQueue;
+    }
+
+    public void scheduleFCFS() {
+        int currentTime = 0;
+        for (PCB pcb : readyQueue) {
+            pcb.waitingTime = currentTime;
+            pcb.turnaroundTime = currentTime + pcb.burstTime;
+            currentTime += pcb.burstTime;
+            System.out.println("Process " + pcb.processId + " starts at " + pcb.waitingTime 
+                                + " and finishes at " + pcb.turnaroundTime);
+        }
+    }
+}  
