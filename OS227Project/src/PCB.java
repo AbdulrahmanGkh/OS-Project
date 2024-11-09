6398286f6aa881ef857a606bc8faf550cfc79f3f
HP <HP@DESKTOP-9VBP18A.lan>
2024-11-09 07:15:52 -0800

    Adding pcb class

--- OS227Project/src/PCB.java
+++ OS227Project/src/PCB.java
@@ -1,4 +1,18 @@
+class PCB {
+    int processId;
+    int burstTime;
+    int memoryRequired;
+    int waitingTime;
+    int turnaroundTime;
+    String state;
 
-public class PCB {
+   
+    public PCB(int processId, int burstTime, int memoryRequired) {
+        this.processId = processId;
+        this.burstTime = burstTime;
+        this.memoryRequired = memoryRequired;
+        this.state = "New"; 
+    }
+    
 
 }
