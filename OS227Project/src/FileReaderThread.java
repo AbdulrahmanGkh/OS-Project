32a68e30455ad4b4197c8bd6c338f25206627f93
Abdullah Alshammari <145904885+Abdu114hf16@users.noreply.github.com>
2024-11-09 22:31:21 +0300

    Update FileReaderThread.java

--- OS227Project/src/FileReaderThread.java
+++ OS227Project/src/FileReaderThread.java
@@ -5,29 +5,27 @@ import java.util.Queue;
 
 class FileReaderThread extends Thread {
     private Queue<PCB> jobQueue;
-    private String fileName;
 
-    public FileReaderThread(Queue<PCB> jobQueue, String fileName) {
+    public FileReaderThread(Queue<PCB> jobQueue) {
         this.jobQueue = jobQueue;
-        this.fileName = fileName;
     }
 
     @Override
     public void run() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("job.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(":|;"); // Assuming format "ID:burstTime;memory"
+                String[] parts = line.split(":|;"); // Spliting each process's info: "ID:burstTime;memory"
                 if (parts.length == 3) {
-                    int id = Integer.parseInt(parts[0]);
-                    int burstTime = Integer.parseInt(parts[1]);
-                    int memoryRequired = Integer.parseInt(parts[2]);
+                    int id = Integer.parseInt(parts[0]); // parts[0] = process id 
+                    int burstTime = Integer.parseInt(parts[1]); // parts[1] = burst time
+                    int memoryRequired = Integer.parseInt(parts[2]); // parts[2] = memoryRequired
 
-                    // Create a new PCB object and add it to the job queue
-                    PCB job = new PCB(id, burstTime, memoryRequired);
-                    jobQueue.add(job);
+                    // Now we have the essential information about a process , create a PCB
+                    PCB process = new PCB(id, burstTime, memoryRequired);
+                    jobQueue.add(process);
 
-                    System.out.println("Job " + job.id + " added to job queue with burst time: " + job.burstTime + " and memory required: " + job.memoryRequired + "MB");
+                    System.out.println("Job " + process.id + " added to job queue with burst time: " + process.burstTime + " and memory required: " + process.memoryRequired + "MB");
                 }
             }
         } catch (IOException e) {
