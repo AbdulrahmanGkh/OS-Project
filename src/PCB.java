6014a96177483ee3241ee5497413898f3d0d3ce3
Abdullah Alshammari <145904885+Abdu114hf16@users.noreply.github.com>
2024-11-20 16:26:18 +0300

    Update PCB.java

--- src/PCB.java
+++ src/PCB.java
@@ -1,17 +1,51 @@
 public class PCB {
-    int id;                  // Process ID
-    int burstTime;           // Execution time (in milliseconds)
-    int remainingTime;       // Remaining burst time (for RR and SJF)
-    int memoryRequired;      // Memory required in MB
-    int waitingTime;         // Waiting time
-    int turnaroundTime;      // Turnaround time
-    String state;            // State of the process (e.g., "Ready", "Running")
+    int id; 			// Process ID
+    int burstTime;      // Process Burst-Time      
+    int remainingTime;  // Time Remaining for the time completion (Helps is RR algorithm )    
+    int memoryRequired; // Process size of memory
+    int waitingTime;	 
+    int turnaroundTime; 
+    String state;
 
     public PCB(int id, int burstTime, int memoryRequired) {
         this.id = id;
-        this.burstTime = burstTime;
-        this.remainingTime = burstTime;
+        this.burstTime = burstTime; 
         this.memoryRequired = memoryRequired;
-        this.state = "New"; // Initial state
+        
+        remainingTime = burstTime;
+        waitingTime = 0;
+        turnaroundTime = 0;
+        state = "NEW";  
+        }
+
+    public void changeState(String state) {
+        this.state = state;
     }
+
+    //Setters & Getters
+	public int getRemainingTime() {
+		return remainingTime;
+	}
+
+	public void setRemainingTime(int remainingTime) {
+		this.remainingTime = remainingTime;
+	}
+
+	public int getWaitingTime() {
+		return waitingTime;
+	}
+
+	public void setWaitingTime(int waitingTime) {
+		this.waitingTime = waitingTime;
+	}
+
+	public int getTurnaroundTime() {
+		return turnaroundTime;
+	}
+
+	public void setTurnaroundTime(int turnaroundTime) {
+		this.turnaroundTime = turnaroundTime;
+	}
+
+    
 }
